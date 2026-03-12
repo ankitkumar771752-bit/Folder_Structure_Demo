ef3d24bcd14b340b6971a4a1e9452f35871c8b66
Sumit Patel <sumitpateledugaon9507@gmail.com>
2026-03-12 14:17:05 +0530

    i worked on git ignore

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-
 require("dotenv").config();
 const app = require("./src/app");
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
-
-
 
 // const express=require('express');
 // require('dotenv').config();
